54aa3408b8eef0d991c43659242c3439d5a875c8
Travis Stewart <ottsel62@https://github.tamu.edu>
2018-03-22 10:03:40 -0500

    Latest front-end code

---
+++ AnomalyDetector/AnomalyDetector/ProcessForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnamolyDetector
+{
+    public partial class ProcessForm : Form
+    {
+        private string workingDirectory;
+        private string batchesDirectory;
+        private string currentBatch;
+        private string nextBatchName;   //The next default batch name
+        private List<string> batch_names = new List<string>();
+
+        public ProcessForm()
+        {
+            InitializeComponent();
+            workingDirectory = Environment.CurrentDirectory;
+            batchesDirectory = Path.Combine(workingDirectory, "..\\Batches");     //Use this if we are using an installer
+            //batchesDirectory = Path.Combine(workingDirectory, "\\Batches");
+            Console.Out.WriteLine(batchesDirectory);
+            
+            
+
+        }
+
+
+        private string getNextBatchName()
+        {
+            string[] batch_paths = Directory.GetDirectories(batchesDirectory);
+            int ct = 0;
+
+            foreach (string str_path in batch_paths)
+            {
+                string str = str_path.Split('\\').Last<string>();
+
+                if (str.StartsWith("Batch_"))
+                {
+                    if (ct < Convert.ToInt32(str.Split('_')[1]))
+                    {
+                        ct = Convert.ToInt32(str.Split('_')[1]);
+                    }
+                }
+            }
+
+            ct++;
+            //nextBatchName = "Batch_" + ct.ToString();
+
+            return "Batch_" + ct.ToString();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            batchName curr = new batchName(getNextBatchName());
+            if (curr.ShowDialog() == DialogResult.OK)
+            {
+                String batchName = curr.getText();
+                currentBatch = batchesDirectory + "\\" + batchName;
+
+                String copyDir = batchesDirectory + "\\" + batchName + "\\Copy";
+                String detDir = batchesDirectory + "\\" + batchName + "\\Detected";
+                String othDir = batchesDirectory + "\\" + batchName + "\\Other";
+
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "image files (*.jpg, *.jpeg, *.png)|*.jpg; *.jpeg; *.png|All files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 1;
+                openFileDialog1.RestoreDirectory = true;
+                openFileDialog1.Multiselect = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //Only create the directories when a file has been selected
+                    Directory.CreateDirectory(currentBatch);
+                    Directory.CreateDirectory(copyDir);
+                    Directory.CreateDirectory(detDir);
+                    Directory.CreateDirectory(othDir);
+
+                    String path;
+                    int fileCt = 0;
+                    foreach (String file in openFileDialog1.FileNames)
+                    {
+                        if (file.EndsWith(".jpg") || file.EndsWith(".jpeg") || file.EndsWith(".png"))
+                        {
+                            path = Path.Combine(copyDir, Path.GetFileName(file));
+                            System.IO.File.Copy(file, path, true);
+                            fileCt++;
+                        }
+                    }
+
+                    filesSelected.Text = "Files Selected: " + fileCt;
+                }
+            }
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            batchName curr = new batchName(getNextBatchName());
+            if (curr.ShowDialog() == DialogResult.OK)
+            {
+
+                String batchName = curr.getText();
+                currentBatch = batchesDirectory + "\\" + batchName;
+
+                String copyDir = batchesDirectory + "\\" + batchName + "\\Copy";
+                String detDir = batchesDirectory + "\\" + batchName + "\\Detected";
+                String othDir = batchesDirectory + "\\" + batchName + "\\Other";
+
+
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //Only create the directories when a folder has been selected
+                    Directory.CreateDirectory(currentBatch);
+                    Directory.CreateDirectory(copyDir);
+                    Directory.CreateDirectory(detDir);
+                    Directory.CreateDirectory(othDir);
+
+                    String path;
+
+                    String[] FileNames = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                    int fileCt = 0;
+                    foreach (String file in FileNames)
+                    {
+                        if (file.EndsWith(".jpg") || file.EndsWith(".jpeg") || file.EndsWith(".png"))
+                        {
+                            path = Path.Combine(copyDir, Path.GetFileName(file));
+                            System.IO.File.Copy(file, path, true);
+                            fileCt++;
+                        }
+                    }
+
+                    filesSelected.Text = "Files Selected: " + fileCt;
+                }
+            }
+        }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            String backendPath = workingDirectory + "\\bin\\dist\\analyze\\analyze.exe";
+            String backendArgs = "-F " + currentBatch;
+
+            if(currentBatch != null)
+            {
+                ProcessStartInfo startConfig = new ProcessStartInfo(backendPath, backendArgs);
+                startConfig.UseShellExecute = false;
+                startConfig.RedirectStandardOutput = true;
+                startConfig.RedirectStandardError = false;
+                startConfig.CreateNoWindow = true;
+
+                Process backendProcess = new Process();
+                backendProcess = Process.Start(startConfig);
+
+               
+
+                //Progess bar updates
+                List<String> completedFiles = new List<string>();
+                while (!backendProcess.HasExited)
+                {
+                    
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ AnomalyDetector/AnomalyDetector/batchName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnamolyDetector
+{
+    public partial class batchName : Form
+    {
+        private string batch_name;
+
+        public batchName(string default_batch_name)
+        {
+            InitializeComponent();
+
+            batch_name = default_batch_name;
+
+            textBox1.Text = batch_name;
+        }
+
+        private void batchName_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            batch_name = textBox1.Text;
+            this.Close();
+        }
+
+        public String getText()
+        {
+            return batch_name;
+        }
+
+    }
+}
