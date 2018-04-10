6e5f71519928a883c4a120382d1b774a28904492
Travis Stewart <ottsel62@https://github.tamu.edu>
2018-04-10 08:02:03 -0500

    Added to the ParametersForm

--- AnomalyDetector/AnomalyDetector/ParametersForm.cs
+++ AnomalyDetector/AnomalyDetector/ParametersForm.cs
@@ -12,14 +12,33 @@ namespace AnomalyDetector
 {
     public partial class ParametersForm : Form
     {
+        public List<string> parameters { get; set; }
+
+
         public ParametersForm()
         {
             InitializeComponent();
+
+            //Read the parameters.ini file and load into form
+            Read();
+        }
+
+        //Read the parameters.ini file
+        private void Read()
+        {
+
+
+        }
+
+        //Save to the parameters.ini file
+        private void Save()
+        {
+
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-
+            Save();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
