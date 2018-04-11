ef13af52594bae852e11bb67ec8b3460d6e506ec
Travis Stewart <ottsel62@https://github.tamu.edu>
2018-04-11 13:38:29 -0500

    Added edit parameter support, updated the installer

--- AnomalyDetector/AnomalyDetector/ParametersForm.Designer.cs
+++ AnomalyDetector/AnomalyDetector/ParametersForm.Designer.cs
@@ -28,15 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.paramData = new System.Windows.Forms.DataGridView();
+            this.Default = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Parameter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saveStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.paramData)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSave
             // 
             this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSave.Location = new System.Drawing.Point(511, 623);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(112, 32);
             this.btnSave.TabIndex = 0;
@@ -56,17 +66,93 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // paramData
+            // 
+            this.paramData.AllowUserToAddRows = false;
+            this.paramData.AllowUserToDeleteRows = false;
+            this.paramData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.paramData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.paramData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.paramData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Default,
+            this.Parameter,
+            this.Value});
+            this.paramData.Location = new System.Drawing.Point(12, 12);
+            this.paramData.Name = "paramData";
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.paramData.RowHeadersDefaultCellStyle = dataGridViewCellStyle8;
+            this.paramData.RowHeadersVisible = false;
+            this.paramData.Size = new System.Drawing.Size(732, 594);
+            this.paramData.TabIndex = 2;
+            this.paramData.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.paramData_CellValueChanged);
+            // 
+            // Default
+            // 
+            this.Default.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Default.FillWeight = 15F;
+            this.Default.HeaderText = "Use Default";
+            this.Default.Name = "Default";
+            // 
+            // Parameter
+            // 
+            this.Parameter.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Parameter.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Parameter.FillWeight = 45F;
+            this.Parameter.HeaderText = "Parameter";
+            this.Parameter.Name = "Parameter";
+            this.Parameter.ReadOnly = true;
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Value.DefaultCellStyle = dataGridViewCellStyle7;
+            this.Value.FillWeight = 40F;
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            // 
+            // saveStatus
+            // 
+            this.saveStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveStatus.Location = new System.Drawing.Point(12, 627);
+            this.saveStatus.Name = "saveStatus";
+            this.saveStatus.Size = new System.Drawing.Size(492, 25);
+            this.saveStatus.TabIndex = 3;
+            this.saveStatus.Text = "Save Status";
+            this.saveStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // ParametersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(756, 668);
+            this.Controls.Add(this.saveStatus);
+            this.Controls.Add(this.paramData);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ParametersForm";
             this.Text = "ParametersForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ParametersForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.paramData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -75,5 +161,10 @@
 
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView paramData;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Default;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Parameter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.Label saveStatus;
     }
 }
--- AnomalyDetector/AnomalyDetector/ParametersForm.cs
+++ AnomalyDetector/AnomalyDetector/ParametersForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,8 @@ namespace AnomalyDetector
 {
     public partial class ParametersForm : Form
     {
-        public List<string> parameters { get; set; }
+        public List<string> paramList { get; set; }
+        public bool saved_changes = false;
 
 
         public ParametersForm()
@@ -21,29 +23,86 @@ namespace AnomalyDetector
 
             //Read the parameters.ini file and load into form
             Read();
+            saveStatus.Text = "Not Saved...";
         }
 
         //Read the parameters.ini file
         private void Read()
         {
+            string[] entries = File.ReadAllLines(@"bin\parameters.ini");
+            foreach (string str in entries)
+            {
+                //Skip comment lines
+                if (str.StartsWith("#")) continue;
 
+                //Remove any remaining comment sections
+                string p_str = str.Split('#')[0];
+
+                string[] opt = p_str.Split('=');      //name=default=value
+                paramData.Rows.Add( Convert.ToBoolean(opt[1]), opt[0], Convert.ToDouble(opt[2]) );
+            }
 
         }
 
         //Save to the parameters.ini file
         private void Save()
         {
+            if (paramData.Rows.Count > 0)
+            {
+                //Create array of strings
+                foreach (DataGridViewRow row in paramData.Rows)
+                {
+                    paramList.Add(row.Cells[1].Value.ToString() + "=" + row.Cells[0].Value.ToString() + "=" + row.Cells[2].Value.ToString());
+                }
+
+                //Write string array to file
+                File.WriteAllLines(@"bin\parameters.ini", paramList.ToArray());
+            }
+
+            saved_changes = true;
+            saveStatus.Text = "Saved...";
 
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
             Save();
+            this.Close();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
+            if(!saved_changes)
+            {
+                DialogResult msg = MessageBox.Show("There are unsaved changes. Would you like to save before exitting?", "Save Changes", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
 
+                if (msg == DialogResult.Yes)
+                    Save();
+                else if (msg == DialogResult.No)
+                    this.Close();
+            }
+            else
+            {
+                this.Close();
+            }
+           
+        }
+
+        private void paramData_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //Something was changed
+            saved_changes = false;
+            saveStatus.Text = "Not Saved...";
+        }
+
+        private void ParametersForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Last chance to save
+            if (!saved_changes)
+            {
+                if (MessageBox.Show("Any unsaved changes will be lost. Would you like to save before exitting?", "Save Changes", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    Save();
+            }
         }
     }
 }
